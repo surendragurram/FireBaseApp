1cfbc4b3c384ebf58085227ba2d04343e5215d15
surendragurram <sgurram36@gmail.com>
2017-03-14 16:56:35 +0100

    config file of firebase

---
+++ js/config.js
@@ -0,0 +1,12 @@
+  // Initialize Firebase
+  // TODO: Replace with your project's customized code snippet
+  (function() {
+   var config = {
+    apiKey: "AIzaSyCsR1W7ekS_ZnjLubgXXTC5Ck0kDIKqOlI",
+    authDomain: "red-orb-124017.firebaseapp.com",
+    databaseURL: "https://red-orb-124017.firebaseio.com",
+    storageBucket: "red-orb-124017.appspot.com",
+    messagingSenderId: "533134319920"
+    };
+  firebase.initializeApp(config);
+}());
